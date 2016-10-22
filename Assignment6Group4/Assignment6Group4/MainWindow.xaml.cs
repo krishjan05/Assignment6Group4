1c5ce316d34b471532bfe50f11ee2ca71501324e
krishjan05 <krishjan05@gmail.com>
2016-10-22 01:00:26 -0400

    Implemented the code for calculating the present value using the future value, time and rate of interest.

---
+++ Assignment6Group4/Assignment6Group4/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment6Group4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double presentAmount;
+        double futureAmount;
+        double duration;
+        double roi;
+        public MainWindow()
+        {
+            InitializeComponent();           
+        }
+        public double CalcPresentValue(double futureAmt,double time, double rateOfInterest)
+        {
+                double numerator = futureAmt;
+                double denomiator = Math.Pow((1 + rateOfInterest), time);
+                presentAmount = numerator / denomiator;
+                return presentAmount;               
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            if (Double.TryParse(txtFutureAmt.Text, out futureAmount) &&
+                Double.TryParse(txtTime.Text, out duration) &&
+                Double.TryParse(txtROI.Text, out roi))
+            {
+                lblPresentAmt.Content = CalcPresentValue(futureAmount, duration, roi);
+            }
+                
+        }
+    }
+}
